c5d038e347cffd1f41e71b3594e47aaff6efe9bf
Maxim Predybailo <predmaxim@ya.ru>
2023-03-20 04:01:57 +0500

    refactor: make some properties required

--- src/utils/productsData.ts
+++ src/utils/productsData.ts
@@ -3,12 +3,12 @@ export interface Product {
   title: string;
   description: string;
   price: number;
-  discountPercentage: number;
-  rating: number;
-  stock: number;
-  brand: string;
-  category: string;
-  thumbnail: string;
+  discountPercentage?: number;
+  rating?: number;
+  stock?: number;
+  brand?: string;
+  category?: string;
+  thumbnail?: string;
   images: string[];
 }
 
