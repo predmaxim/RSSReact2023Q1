b3889c7701984729077d01dd72f689c2b5686c98
Maxim Predybailo <predmaxim@ya.ru>
2023-03-19 23:17:10 +0500

    refactor: restructuring App

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,18 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import './App.css';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { Home, Page404 } from './pages';
+import { Header } from './components';
 
-function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <div className="App">
+export default class App extends React.Component {
+  render() {
+    return (
       <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<Page404 />} />
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </div>
-  );
+    );
+  }
 }
-
-export default App;
