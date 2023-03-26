114d64b79386de7e70d77429b3085665cf99d8e7
Maxim Predybailo <predmaxim@ya.ru>
2023-03-27 00:04:08 +0500

    feat: add functions to utils

---
+++ src/utils/index.ts
@@ -0,0 +1,3 @@
+export { setLabel } from './setLabel';
+export { upperCaseFirstLetter } from './upperCaseFirstLetter';
+export { validateFirstLetter, validateEmpty, validateChecked } from './formValidation';
