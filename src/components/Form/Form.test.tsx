6ebef0571bd6b5da8f490e33112f492b17b8dca9
Maxim Predybailo <predmaxim@ya.ru>
2023-04-03 04:58:13 +0500

    feat: add testing-library__jest-dom

--- src/components/Form/Form.test.tsx
+++ src/components/Form/Form.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { beforeEach, describe, expect, test } from 'vitest';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import { Form } from './Form';
 
 const updateFormPageState = (form: JSX.Element): void => {
