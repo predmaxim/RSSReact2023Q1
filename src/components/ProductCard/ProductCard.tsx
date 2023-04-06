4fb1695fe0207f8b88158463f912db360b0c2f54
Maxim Predybailo <predmaxim@ya.ru>
2023-04-07 00:19:38 +0500

    feat: add some chenges:
    - add some props
    - add quick view handling
    - change some layout

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,17 +1,41 @@
-import React from 'react';
-import { Product } from '../../utils/productsData';
+import React, { useState } from 'react';
 import style from './ProductCard.module.css';
+import { Product } from '../../utils/api';
+import { QuickView } from '../../components';
 
-export function ProductCard({ id, images, title, description, price }: Product) {
+export function ProductCard({
+  id,
+  images,
+  title,
+  description,
+  price,
+  rating,
+  category,
+  stock,
+}: Product) {
+  const [quickView, setQuickView] = useState(false);
+
+  const onClickHandler = () => {
+    setQuickView(true);
+  };
+
   return (
-    <div className={style.card} id={`${id}`}>
-      <img className={style.img} src={images[0]} alt={title} loading="lazy" />
-      <h3 className={style.title}>{title}</h3>
-      <p className={style.description}>{description}</p>
-      <div className={style.cardFooter}>
-        <p className={style.price}>{`$${price}`}</p>
-        <button>Add to Cart</button>
+    <>
+      {quickView && (
+        <QuickView
+          setQuickView={setQuickView}
+          product={{ id, images, title, description, price, rating, category, stock }}
+        />
+      )}
+      <div className={style.card} id={`${id}`}>
+        <div className={style.imgContainer}>
+          <a href="#" onClick={onClickHandler}>
+            <img className={style.img} src={images[0]} alt={title} loading="lazy" />
+          </a>
+        </div>
+        <h3 className={style.title}>{title}</h3>
+        <p className={style.description}>{description}</p>
       </div>
-    </div>
+    </>
   );
 }
