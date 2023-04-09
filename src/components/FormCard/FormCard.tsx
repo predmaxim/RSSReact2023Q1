407d2e058b661de5f2b01921f8d312f804a9cede
Maxim Predybailo <predmaxim@ya.ru>
2023-04-10 01:29:25 +0500

    feat: add img in card for file input

--- src/components/FormCard/FormCard.tsx
+++ src/components/FormCard/FormCard.tsx
@@ -23,7 +23,7 @@ export function FormCard({
 }: FormCardProps) {
   return (
     <div className={style.card} id={`${id}`}>
-      <p>{avatar}</p>
+      <img src={avatar} alt={firstName} width="200" />
       <h3 className={style.name}>
         {firstName} {lastName}
       </h3>
