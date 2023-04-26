723c70854feb111ef6ad5f94f4250cb4e8d8c2fa
Maxim Predybailo <predmaxim@ya.ru>
2023-04-26 08:46:05 +0500

    feat: add baseUrl to cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,16 +1,18 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
+  projectId: 'uk8kuu',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    baseUrl: 'http://localhost:5173',
   },
 
   component: {
     devServer: {
-      framework: "react",
-      bundler: "vite",
+      framework: 'react',
+      bundler: 'vite',
     },
   },
 });
