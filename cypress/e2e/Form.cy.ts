6a3572c47741be8df34adb0b1dc8c012cc1ccddb
Maxim Predybailo <predmaxim@ya.ru>
2023-05-01 18:11:18 +0500

    feat: add some tests

--- cypress/e2e/Form.cy.ts
+++ cypress/e2e/Form.cy.ts
@@ -1,10 +1,28 @@
 /// <reference types="cypress" />
 
-describe('Test Form Page', () => {
-  it('Vitit form page and get Heaader', () => {
+describe('Test Form', () => {
+  beforeEach(() => {
     cy.visit('/');
     cy.contains('Form').click();
     cy.url().should('include', '/form');
     expect(cy.contains('Form Page'));
+  });
+  it('Form is showed', () => {
+    expect(cy.contains('Enter your data'));
+    expect(cy.get('form'));
+    expect(cy.get('input[type="file"]'));
+    expect(cy.get('input[type="text"]').should('have.length', 2));
+    expect(cy.get('input[type="radio"]').should('have.length', 2));
+    expect(cy.get('input[type="checkbox"]'));
+    expect(cy.get('button').should('have.text', 'Submit'));
+  });
+  it('Validation is work', () => {
+    cy.get('button').should('have.text', 'Submit').click();
+    expect(cy.contains('Choose an image'));
+    expect(cy.contains('The field must not be empty'));
+    expect(cy.contains('Select your birthday'));
+    expect(cy.contains('Select your city'));
+    expect(cy.contains('Select your gender'));
+    expect(cy.contains('Accept the agreement!'));
   });
 });
--- cypress/e2e/FormCard.cy.ts
+++ cypress/e2e/FormCard.cy.ts
@@ -1,10 +1,27 @@
 /// <reference types="cypress" />
 
-describe('Test Form Page', () => {
-  it('Vitit form page and get Heaader', () => {
+describe('Test Form Card', () => {
+  beforeEach(() => {
     cy.visit('/');
     cy.contains('Form').click();
     cy.url().should('include', '/form');
     expect(cy.contains('Form Page'));
+  });
+
+  it('Card should be created', () => {
+    cy.get('input[type="file"]').selectFile({
+      contents: Cypress.Buffer.from('test'),
+      fileName: 'test.jps',
+      lastModified: Date.now(),
+    });
+
+    cy.get('input[name="firstName"]').type('Name');
+    cy.get('input[name="lastName"]').type('LastName');
+    cy.get('input[name="birthday"]').type('1985-05-06');
+    cy.get('select[name="city"]').select('Minsk');
+    cy.get('#male').click();
+    cy.get('input[name="agreement"]').click();
+    cy.get('button').click();
+    expect(cy.get('h3').should('have.text', 'Name LastName'));
   });
 });
