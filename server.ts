53f1f45460acd1cf1ca3e5f020a09f75cd8cea64
Maxim Predybailo <predmaxim@ya.ru>
2023-05-03 14:23:00 +0500

    fix: catch any type

--- server.ts
+++ server.ts
@@ -1,24 +1,18 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import fs from 'node:fs/promises';
 import express from 'express';
 import { ViteDevServer } from 'vite';
-import { server } from 'typescript';
 
-// Constants
 const isProduction = process.env.NODE_ENV === 'production';
 const port = process.env.PORT || 5173;
 const base = process.env.BASE || '/';
 
-// Cached production assets
 const templateHtml = isProduction ? await fs.readFile('./dist/client/index.html', 'utf-8') : '';
 const ssrManifest = isProduction
   ? await fs.readFile('./dist/client/ssr-manifest.json', 'utf-8')
   : undefined;
 
-// Create http server
 const app = express();
 
-// Add Vite or respective production middlewares
 let vite: ViteDevServer;
 if (!isProduction) {
   const { createServer } = await import('vite');
@@ -35,16 +29,14 @@ if (!isProduction) {
   app.use(base, sirv('./dist/client', { extensions: [] }));
 }
 
-// Serve HTML
 app.use('*', async (req, res) => {
   try {
     const url = req.originalUrl.replace(base, '');
 
     let template: string;
-    let render: (arg0: string, arg1: string | undefined) => any;
+    let render;
 
     if (!isProduction) {
-      // Always read fresh template in development
       template = await fs.readFile('./index.html', 'utf-8');
       template = await vite.transformIndexHtml(url, template);
       render = (await vite.ssrLoadModule('/src/entry-server.tsx')).render;
@@ -61,6 +53,7 @@ app.use('*', async (req, res) => {
       .replace(`<!--app-html-->`, rendered.html ?? '');
 
     res.status(200).set({ 'Content-Type': 'text/html' }).end(html);
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
   } catch (e: any) {
     vite?.ssrFixStacktrace(e);
     console.log(e.stack);
@@ -68,7 +61,6 @@ app.use('*', async (req, res) => {
   }
 });
 
-// Start http server
 app.listen(port, () => {
   console.log(`Server started at http://localhost:${port}`);
 });
